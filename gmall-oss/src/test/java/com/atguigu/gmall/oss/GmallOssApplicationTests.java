9da884786844f3c15fca96bde5d42383e2893f38
yubinshensyb <2493599153@qq.com>
2020-12-15 10:09:52 +0800

    创建后台工程

---
+++ gmall-oss/src/test/java/com/atguigu/gmall/oss/GmallOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.oss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallOssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
